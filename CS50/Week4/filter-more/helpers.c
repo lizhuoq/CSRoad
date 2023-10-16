7175260449beb674bd22b9413014ad2a6e874c1f
lizq <100779955+lizhuoq@users.noreply.github.com>
2023-10-16 01:48:07 +0000

    Mon, Oct 16, 2023, 9:48 AM +08:00

--- CS50/Week4/filter-more/helpers.c
+++ CS50/Week4/filter-more/helpers.c
@@ -21,7 +21,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        RGBTRIPLE *image_copy = 
+        RGBTRIPLE *image_copy = malloc(width);
         for (int j = 0; j < width; j++)
         {
             image
