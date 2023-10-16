0a2a7a6eafc471664245a2fb535aa5c128596fbb
lizq <100779955+lizhuoq@users.noreply.github.com>
2023-10-16 02:08:41 +0000

    Mon, Oct 16, 2023, 10:08 AM +08:00

--- CS50/Week4/filter-more/helpers.c
+++ CS50/Week4/filter-more/helpers.c
@@ -38,6 +38,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE image_copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
