078e93dd208e4d42a5a99712df075c0068903fda
lizq <100779955+lizhuoq@users.noreply.github.com>
2023-10-16 01:33:51 +0000

    Mon, Oct 16, 2023, 9:33 AM +08:00

--- CS50/Week4/filter-more/helpers.c
+++ CS50/Week4/filter-more/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE average = (image[i][j].rgbtBlue + iamge[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            BYTE average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
